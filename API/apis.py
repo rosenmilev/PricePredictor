9082ec95b69c089cb2dea622b910b55c6b5447d1
traxx900 <rosenmilev@gmail.com>
2024-02-28 21:56:15 +0200

    Design and develop AlphaVantageApi class, which is responsible for all API calls to Alpha Vantage.

--- API/apis.py
+++ API/apis.py
@@ -4,76 +4,69 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import plotly.express as px
+import matplotlib.pyplot as plt
+from datetime import date
+import json
 
 
+# def get_crypto_data(fetcher, symbol, market):
+# 	data = fetcher(symbol, market)
+# 	data_df = pd.DataFrame(data).T
+#
+#
 key = '30DQX6JQOX72GQ4E'
-ts = TimeSeries(key=key)
+# ts = TimeSeries(key=key, output_format='pandas')
+#
+# data, metadata = ts.get_daily(symbol='IBM', outputsize='full')
 
 
-# Create function to fetch historical data from API.
-def get_historical_data(symbol, start_date, end_date):
-	data, meta_data = ts.get_daily(symbol, outputsize='full')
-	df = pd.DataFrame(data).T
-	df.columns = ['open', 'high', 'low', 'close', 'volume']
-	df.index = pd.to_datetime(df.index)
-	df.sort_index(inplace=True)
-
-	name = f'{symbol}_{start_date}_{end_date}'
-
-	result = df[start_date:end_date]
-	result.to_csv(name)
-
-	return result
+def save_data(name, format, data):
+	if format == 'json':
+		current_date = date.today()
+		file_path = f'{name}_{current_date}.json'
+		with open(file_path, 'w') as f:
+			json.dump(data, f)
+		return print(f'File saved as: {file_path}')
 
 
+def retrieve_data(file_path, format):
+	if format == 'json':
+		with open(file_path, 'r') as f:
+			retrieved_data = json.load(f)
+	return retrieved_data
 
 
-start_date = '2023-01-01'
-end_date = '2023-12-31'
+class AlphaVantageApi:
+	def __init__(self, key):
+		self.key = key
 
-gold_data = get_historical_data('GOLD', start_date, end_date)
-oil_data = get_historical_data('CL=F', start_date, end_date)
+	def fetch_data(self, url):
+		r = requests.get(url)
+		data = r.json()
+		return data
 
-gold_data_file_path = 'GLD_2023-01-01_2023-12-31'
-gold_df = pd.read_csv(gold_data_file_path, index_col=0, parse_dates=True)
-print(gold_df)
+	def top_gainers_losers(self):
+		url = f'https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey={self.key}'
 
-oil_data_file_path = 'OIL_2023-01-01_2023-12-31'
-oil_df = pd.read_csv(oil_data_file_path, index_col=0, parse_dates=True)
-print(oil_df)
+		return self.fetch_data(url)
 
-# Dataframes for oil and gold prices. The selected indicator is closing price
-# Filter both dataframes based on common dates
 
-common_dates = gold_df.index.intersection(oil_df.index)
-print(len(common_dates))
+	def news_sentiment(self, tickers, date_from, topics=None, limit=10, sort='LATEST'):
 
-oil_data = oil_df.loc[common_dates, ['close']]
-oil_data = oil_data.rename(columns={'close': 'oil_price'})
-gold_data = gold_df.loc[common_dates, ['close']]
-gold_data = gold_data.rename(columns={'close': 'gold_price'})
-merged_data = pd.concat([oil_data, gold_data], axis=1)
+		url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&' \
+			  f'&time_from={date_from}apikey={self.key}&limit={limit}%sort={sort}'
+		if tickers:
+			url += f'&tickers={tickers}'
+		elif topics:
+			url += f'&topics={topics}'
+		return self.fetch_data(url)
 
-print(merged_data)
-# Calculate correlation between two
-correlation = merged_data.oil_price.corr(merged_data.gold_price)
-print(correlation)
-# It appears there is weak negative correlation of -0.183
-# Visualize the correlation
 
-fig = px.line(merged_data, x=merged_data.index, y=['oil_price', 'gold_price'], labels={'value': 'Price', 'variable': 'Asset'},
-              title='Oil and Gold Prices Over Time')
 
-fig.update_layout(xaxis_title='Date', yaxis_title='Price')
-# fig.show()
+top_worst_performers = retrieve_data('top_gainers_2024-02-28.json', 'json')
+df_top_gainers = pd.DataFrame(top_worst_performers['top_gainers'])
+df_top_losers = pd.DataFrame(top_worst_performers['top_losers'])
+df_most_actively_traded = pd.DataFrame(top_worst_performers['most_actively_traded'])
 
-input, target = merged_data[['oil_price']], merged_data['gold_price']
 
-model = LinearRegression()
-model.fit(input, target)
-
-predictions = model.predict(input)
-
-mse = mean_squared_error(target, predictions)
-print(mse)
 
